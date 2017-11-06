fe66c2595bfab678de46f402f4367a320f8a897e
afridrikh <oooahtoh@gmail.com>
2017-11-07 00:40:34 +0300

    modified sql_insert
    modified sql_insert

--- Index.php
+++ Index.php
@@ -46,8 +46,7 @@
         $email = $_POST['email']; 
         $date = date("Y-m-d"); 
         // Insert data 
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)"; 
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date) VALUES (?,?,?)"; 
         $stmt = $conn->prepare($sql_insert); 
         $stmt->bindValue(1, $name); 
         $stmt->bindValue(2, $email); 
